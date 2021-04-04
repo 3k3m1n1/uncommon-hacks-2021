08a32a0077f9de082ce1c91ab6bc213968b32312
Ekemini Nkanta <3k3m1n1@gmail.com>
2021-04-04 10:51:43 -0400

    audio visualizer: simplified 512 frequency bars to 8

---
+++ 3d-music-visualizer/Assets/Scripts/AudioReactiveCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReactiveCube : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMultiplier;
+
+    public AudioVisualizer audioVisualizer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (audioVisualizer.frequencyBand[band] * scaleMultiplier) + startScale, transform.localScale.z);
+    }
+}
--- 3d-music-visualizer/Assets/Scripts/AudioVisualizer.cs
+++ 3d-music-visualizer/Assets/Scripts/AudioVisualizer.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+// credits to Peer Play's "Audio Visualization" series on YouTube! https://www.youtube.com/watch?v=5pmoP1ZOoNs&list=PL3POsQzaCw53p2tA6AWf7_AWgplskR0Vo
 
 [RequireComponent (typeof (AudioSource))]
 public class AudioVisualizer : MonoBehaviour
 {
     AudioSource audioSource;
-    public float[] samples = new float[512]; 
+    public float[] samples = new float[512];
+    public float[] frequencyBand = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +22,44 @@ public class AudioVisualizer : MonoBehaviour
     void Update()
     {
         audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+        MakeFrequencyBands();
+    }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         * 22050 Hz / 512 samples = 43 Hz per sample
+         *
+         * 20 - 60 hertz
+         * 60 - 250 hertz
+         * 250 - 500 hertz
+         * 500 - 2000 hertz
+         * 2000 - 4000 hertz
+         * 4000 - 6000 hertz
+         * 6000 - 20000 hertz
+         *
+         */
+
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            frequencyBand[i] = average * 10;
+        }
     }
 }
