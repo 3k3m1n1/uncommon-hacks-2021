925093c70842f88d2a83286801c22ece7f2a57ac
Ekemini Nkanta <3k3m1n1@gmail.com>
2021-04-04 10:31:49 -0400

    example audio visualizer is working! gonna hook it up to the lights next

---
+++ 3d-music-visualizer/Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioVisualizer : MonoBehaviour
+{
+    AudioSource audioSource;
+    public float[] samples = new float[512]; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ 3d-music-visualizer/Assets/Scripts/Instantiate512Cubes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate512Cubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject[] cubes = new GameObject[512];
+    public float maxScale;
+
+    public AudioVisualizer audioVisualizer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject instance = (GameObject)Instantiate(cubePrefab);
+
+            instance.transform.position = this.transform.position;  // zero it out first
+            instance.transform.parent = this.transform;
+            instance.name = "Sample" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instance.transform.position = Vector3.forward * 100;
+            cubes[i] = instance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (cubes != null)
+            {
+                cubes[i].transform.localScale = new Vector3(10, audioVisualizer.samples[i] * maxScale, 10);
+            }
+        }
+    }
+}
